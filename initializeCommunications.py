56b9b93f2cb03694efea00ca4882866b6a71164b
Geoffrey Glangine <geoffrey.glangine@gmail.com>
2017-05-31 15:06:59 +0200

    add shity comments and propering the code

--- initializeCommunications.py
+++ initializeCommunications.py
@@ -1,9 +1,11 @@
 from ovsfGenerator import ovsfGenerator
 from Mobile import Mobile
 
+#convert a number in a string of bits
 def bitToString(a):
     return (bin(a)[2:])
 
+#function which is decoding a signal in the global signal
 def demodulateSignal(sig, cellPhone):
     k=0
     multipliedSignal=[]
@@ -20,13 +22,16 @@ def demodulateSignal(sig, cellPhone):
     else:
         return False
 
-
+#generate Codes
 listCodes = ovsfGenerator(4)
+#generate a testing list of mobile phones
 listMobile = [Mobile("A",listCodes[0]),Mobile("B",listCodes[1]),Mobile("C",listCodes[2]),Mobile("D",listCodes[3])]
 for mobile in listMobile :
     print(mobile)
 
+
 signal = [0,0,0,0]
+#generate the global signal with the mobiles ovsf codes (all mobiles send 1 for the moment
 for i in range(0,4):
     for mobile in listMobile :
         bits=bitToString(mobile.ovsfCode)
@@ -36,26 +41,12 @@ for i in range(0,4):
             signal[i]+=1
 #signal=signal[::-1]
 print(signal)
-message=[0,0,0,0]
-j=0
-data = dict()
 
-
-for mobile in listMobile :
-    data[mobile.identifier]=[0,0,0,0]
-
-for i in signal:
-    for mobile in listMobile :
-        bits=bitToString(mobile.ovsfCode)
-        if(bits[j]=='0'):
-            data[mobile.identifier][j]=1
-        else:
-            data[mobile.identifier][j]=-1
-    j+=1
-for i in data:
-    print(i+" "+str(data[i]))
-
+#cheating function in order to test if the demodulation fuction works
+#Actualy, with this signal the function may return True False False True, True is 1 and False is "You know..."
 signal=[0,0,0,4]
+
+#signal=[0,0,0,4]
 for i in listMobile :
     print(demodulateSignal(signal,i))
 
