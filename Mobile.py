910214a95b43a9425e5c711a5e53332005681b27
Geoffrey Glangine <geoffrey.glangine@gmail.com>
2017-06-06 14:08:37 +0200

    debug for pi

--- Mobile.py
+++ Mobile.py
@@ -1,15 +1,19 @@
+import random
 class Mobile :
 
     def __init__(self, id, ovsf):
         self.ovsfCode = ovsf
         self.identifier = id
-        
+        self.message = random.randint(-1,1)
+
+         
+
     def setOvsfCode(self, ovsf):
         self.ovsfCode = ovsf
     def setId(self, id):
         self.identifier = id
     def __str__(self):
-        return u"\U0001F4F1" + self.identifier + " my code is " + str(bin(self.ovsfCode) + "")
+        return u"\U0001F4F1" + self.identifier + " my code is " + (bin(self.ovsfCode)) + " "+ "my message i "+ str(self.message)
 
 
 
--- initializeCommunications.py
+++ initializeCommunications.py
@@ -1,22 +1,41 @@
 from ovsfGenerator import ovsfGenerator
 from Mobile import Mobile
 
+MOBILE_NUMBER=4
+
 #convert a number in a string of bits
 def bitToString(a):
     return (bin(a)[2:])
+#compute the conjugue of a binary number
+def conjugue(a):
+    conj=""
+    for i in a:
+        if(i=='0'):
+           conj+='1' 
+        else:
+            conj+='0'
+
+    return conj
 
 #function which is decoding a signal in the global signal
 def demodulateSignal(sig, cellPhone):
     
     multipliedSignal=[]
     for i in sig :
+        print (i)
         if(bitToString(cellPhone.ovsfCode)[sig.index(i)]=='1'):
             multipliedSignal.append(i)
+            #print('1',end='')
         else:
             multipliedSignal.append(-i)
+            #print('-1', end='')
     value=0
+    #print()
+    #print(bitToString(cellPhone.ovsfCode))
+    #print(multipliedSignal)
     for i in multipliedSignal:
         value+=i
+    print("####" + str(value))
     if (value > 0):
         return 1
     elif(value==0):
@@ -25,34 +44,39 @@ def demodulateSignal(sig, cellPhone):
         return -1
 
 #generate Codes
-listCodes = ovsfGenerator(4)
+listCodes = ovsfGenerator(MOBILE_NUMBER)
 #generate a testing list of mobile phones
-listMobile = [Mobile("A",listCodes[0]),Mobile("B",listCodes[1]),Mobile("C",listCodes[2]),Mobile("D",listCodes[3])]
+listMobile = [Mobile(str(i),listCodes[i]) for i in range(0,MOBILE_NUMBER) ]
+print(listMobile)
 for mobile in listMobile :
     print(mobile)
 
 
-signal = [0,0,0,0]
+signal = [0 for x in range(0,len(bitToString(listMobile[0].ovsfCode)))]
 #generate the global signal with the mobiles ovsf codes (all mobiles send 1 for the moment
-for i in range(0,4):
+print(len(bitToString(listMobile[0].ovsfCode)))
+for i in range(0,len(bitToString(listMobile[0].ovsfCode))):
     for mobile in listMobile :
-        bits=bitToString(mobile.ovsfCode)
-        if(bits[i]=='0'):
-            signal[i]-=1
-        else:
-            signal[i]+=1
+        if(mobile.message !=0):
+            if(mobile.message == 1):
+                bits=bitToString(mobile.ovsfCode)
+            if(mobile.message == -1):
+                bits=conjugue(bitToString(mobile.ovsfCode))
+            if(bits[i]=='0'):
+                signal[i]-=1
+            else:
+                signal[i]+=1
 #signal=signal[::-1]
 print(signal)
 
 #cheating function in order to test if the demodulation fuction works
 #Actualy, with this signal the function may return 1 -1 -1 1, 1 is 1 and -1 is 0 and 0 is nothing
-signal=[0,0,0,4]
-
-#signal=[0,0,0,4]
+#
+signal=[0,0,2,2]
 for i in listMobile :
     print(demodulateSignal(signal,i))
 
-
+ 
 
 
 
