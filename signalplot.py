e64463f1616e221700d8b0b54fce1f5e3b61f0fb
Paul TREHIOU <paul@nyanlout.re>
2017-06-13 15:40:12 +0200

    Amplitude du signal prise en compte

--- signalplot.py
+++ signalplot.py
@@ -11,9 +11,9 @@ def plot(signal=[-1, 0, 1]):
         t = np.arange(i * 2 * np.pi, (i * 2 + 2) * np.pi, 0.01)
 
         if chip < 0:
-            s = np.sin(t)
+            s = np.sin(t) * abs(chip)
         elif chip > 0:
-            s = np.sin(t - np.pi)
+            s = np.sin(t - np.pi) * abs(chip)
         else:
             s = np.zeros_like(t)
 
