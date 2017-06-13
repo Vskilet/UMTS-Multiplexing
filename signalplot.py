b55fe1061b2b466d537cf686c1a79b78b2059272
Paul TREHIOU <paul@nyanlout.re>
2017-06-13 15:48:10 +0200

    Correction sinusoides

--- signalplot.py
+++ signalplot.py
@@ -11,13 +11,13 @@ def plot(signal=[-1, 0, 1]):
         t = np.arange(i * 2 * np.pi, (i * 2 + 2) * np.pi, 0.01)
 
         if chip < 0:
-            s = np.sin(t) * abs(chip)
-        elif chip > 0:
             s = np.sin(t - np.pi) * abs(chip)
+        elif chip > 0:
+            s = np.sin(t) * abs(chip)
         else:
             s = np.zeros_like(t)
 
-        plt.plot(t, s)
+        plt.plot(t, s, 'b')
         i += 1
 
     plt.savefig("plot.png", format="png")
