8e73cf41f328f05ed785600e7477fb2e50ba409a
Paul TREHIOU <paul@nyanlout.re>
2017-06-13 15:05:04 +0200

    Fonction matplotlib retourne une figure

--- signalplot.py
+++ signalplot.py
@@ -2,20 +2,24 @@ import numpy as np
 import matplotlib.pyplot as plt
 
 
-signal = [-1, 0, 1, 1, -1, -1, 1, 0, 1, 1, 1, -1, 0, 0, 0, 1]
-i = 0
+def plot(signal=[-1, 0, 1]):
 
-for chip in signal:
-    t = np.arange(i * 2 * np.pi, (i * 2 + 2) * np.pi, 0.01)
+    i = 0
 
-    if chip < 0:
-        s = np.sin(t)
-    elif chip > 0:
-        s = np.sin(t - np.pi)
-    else:
-        s = np.zeros_like(t)
+    for chip in signal:
+        t = np.arange(i * 2 * np.pi, (i * 2 + 2) * np.pi, 0.01)
 
-    plt.plot(t, s)
-    i += 1
+        if chip < 0:
+            s = np.sin(t)
+        elif chip > 0:
+            s = np.sin(t - np.pi)
+        else:
+            s = np.zeros_like(t)
 
-plt.show()
+        plt.plot(t, s)
+        i += 1
+
+    plt.show()
+    fig = plt.gcf()
+    # plt.show() <- don't show window!
+    return fig
