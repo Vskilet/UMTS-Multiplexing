6bc2cc15956a160ff2c0503a5f0649377073ec94
Paul TREHIOU <paul@nyanlout.re>
2017-06-13 14:52:52 +0200

    Génération correcte d'un signal fourni

--- signalplot.py
+++ signalplot.py
@@ -2,12 +2,19 @@ import numpy as np
 import matplotlib.pyplot as plt
 
 
-signal = [-1, 0, 1, 1, -1, -1, 1, 0]
+signal = [-1, 0, 1, 1, -1, -1, 1, 0, 1, 1, 1, -1, 0, 0, 0, 1]
 i = 0
 
 for chip in signal:
     t = np.arange(i * 2 * np.pi, (i * 2 + 2) * np.pi, 0.01)
-    s = np.sin(t - np.pi * (i % 2))
+
+    if chip < 0:
+        s = np.sin(t)
+    elif chip > 0:
+        s = np.sin(t - np.pi)
+    else:
+        s = np.zeros_like(t)
+
     plt.plot(t, s)
     i += 1
 
